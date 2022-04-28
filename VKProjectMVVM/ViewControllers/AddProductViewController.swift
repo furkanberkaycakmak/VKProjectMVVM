0c1539e934c62f32215e53f72340f85eb1fe244b
furkanberkaycakmak <furkanberkaycakmak@icloud.com>
2022-04-28 15:27:30 +0300

    App icons added to project

--- VKProjectMVVM/ViewControllers/AddProductViewController.swift
+++ VKProjectMVVM/ViewControllers/AddProductViewController.swift
@@ -2,28 +2,22 @@
 //  AddProductViewController.swift
 //  VKProjectMVVM
 //
-//  Created by Berkay ÇAKMAK on 28.04.2022.
+//  Created by Berkay ÇAKMAK on 26.04.2022.
 //
 
 import UIKit
 
 class AddProductViewController: UIViewController {
-
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet weak var imageUrlTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func addProductButtonClicked(_ sender: Any) {
     }
-    */
-
 }
